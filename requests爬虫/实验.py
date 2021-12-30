c509767654d3d0fa2d289336c127146b0db88aed
朱德帅 <17836938401@139.com>
2021-12-30 14:21:57 +0800

    爬虫学习

--- requests爬虫/实验.py
+++ requests爬虫/实验.py
@@ -1,4 +1,6 @@
 import requests
+from urllib.request import urlopen
+import http.client
 
 header1 = {
     "User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Mobile Safari/537.36"}
@@ -6,3 +8,6 @@ header1 = {
 response = requests.get("https://www.baidu.com",headers = header1)
 
 print(response.content.decode())
+
+r = urlopen("http://baiwu.com") # type:http.client.HTTPResponse
+
